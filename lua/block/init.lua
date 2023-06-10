bd84db1462a9e273484d041f2e4a94d8623c2671
hampus <hampus.hauffman@wearelevels.com>
2023-06-10 15:06:52 +0200

    Ok

--- lua/block/init.lua
+++ lua/block/init.lua
@@ -7,8 +7,6 @@ local options = {
 
 function M.setup(opts)
     print("HELLO")
-    M.options = vim.tbl_deep_extend("force", M.options, opts or {})
-    require("block.util").create_hl(M.options.depth, M.options.start_color)
 end
 
 return setmetatable(M, {
