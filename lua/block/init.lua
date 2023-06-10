837a3341945ce1dbf5b7540cce25537d1156c059
hampus <hampus.hauffman@wearelevels.com>
2023-06-10 14:56:54 +0200

    ok

--- lua/block/init.lua
+++ lua/block/init.lua
@@ -13,11 +13,10 @@ function M.setup(opts)
     opts = vim.tbl_deep_extend("force", M.defaults, opts or {})
     M.options = opts
     require("block.util").create_hl(opts.depth, opts.start_color)
-
-    local b = require("block.block")
-    api.nvim_create_user_command("BlockToggle", b.toggle)
-    api.nvim_create_user_command("BlockOn", b.on)
-    api.nvim_create_user_command("BlockOff", b.off)
 end
 
-return M
+return setmetatable(M, {
+    __index = function(_, k)
+        return require("block.block")[k]
+    end,
+})
--- plugin/block.vim
+++ plugin/block.vim
@@ -2,6 +2,6 @@
 " Description:  
 
 " Exposes the plugin's functions for use as commands in Neovim.
-"command! -nargs=0 Block lua require("block").toggle()
-"command! -nargs=0 BlockOff lua require("block").off()
-"command! -nargs=0 BlockOn lua require("block").on()
+command! -nargs=0 Block lua require("block").toggle()
+command! -nargs=0 BlockOff lua require("block").off()
+command! -nargs=0 BlockOn lua require("block").on()
