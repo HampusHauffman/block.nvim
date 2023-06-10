cff6bfbee90262f5a15c3cd87668c6e42bf39ebe
hampus <hampus.hauffman@wearelevels.com>
2023-06-10 15:03:27 +0200

    Ok

--- lua/block/init.lua
+++ lua/block/init.lua
@@ -6,10 +6,8 @@ local options = {
     depth = 3,
 }
 
-
 function M.setup(opts)
-    opts = vim.tbl_deep_extend("force", M.options, opts or {})
-    M.options = opts
+    M.options = vim.tbl_deep_extend("force", M.options, opts or {})
     require("block.util").create_hl(M.options.depth, M.options.start_color)
 end
 
