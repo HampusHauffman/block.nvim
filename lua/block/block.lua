cbc5b724a45ca2cefa93b0a3954cf6613a6d5cef
TrustlyHampus <hampus.hauffman@trustly.com>
2024-04-09 15:26:12 +0200

    works

--- lua/block/block.lua
+++ lua/block/block.lua
@@ -1,6 +1,6 @@
 local M           = {}
 
---- @type table<integer,{parser:LanguageTree}>
+--- @type table<integer,{parser:LanguageTree, tabstop: integer, matchpairs: string}>
 local buffers     = {}
 local api         = vim.api
 local ts          = vim.treesitter
@@ -21,30 +21,31 @@ end
 ---@param end_row integer The final row to go to
 ---@param end_col integer the column to witch all highligts must reach
 ---@param iteration integer How deep we are in the recursive cycle
-local function color_node(start_row, start_col, end_row, end_col, iteration)
+local function color_node(bufnr, start_row, start_col, end_row, end_col, iteration)
     for i = start_row, end_row do
-        api.nvim_buf_add_highlight(0, ns_id, "Block" .. iteration % nest_amount, i, start_col, end_col)
+        api.nvim_buf_add_highlight(bufnr, ns_id, "Block" .. iteration % nest_amount, i, start_col, end_col)
         -- Highlight that comes after the end of the line
-        local l = vim.api.nvim_buf_get_lines(0, i, i + 1, false)[1]
+        local l = vim.api.nvim_buf_get_lines(bufnr, i, i + 1, false)[1]
+        local l_len = vim.fn.strdisplaywidth(l)
         if (l == nil) then goto continue end -- AH YES THE FORBIDDEN FRUIT TODO fix this. Seems
         if (#l > 0) then
-            api.nvim_buf_set_extmark(0, ns_id, i, 0, {
+            api.nvim_buf_set_extmark(bufnr, ns_id, i, 0, {
                 virt_text = {
-                    { string.rep(" ", end_col - #l),
+                    { string.rep(" ", end_col - l_len),
                         "Block" .. iteration % nest_amount
                     }
                 },
-                virt_text_win_col = #l,
-                priority = 200 + iteration
+                virt_text_win_col = l_len,
+                priority = 100 + iteration
             })
         else
             -- Highlight the empty lines
-            api.nvim_buf_set_extmark(0, ns_id, i, 0, {
+            api.nvim_buf_set_extmark(bufnr, ns_id, i, 0, {
                 virt_text = {
                     { string.rep(" ", end_col - start_col),
                         "Block" .. iteration % nest_amount } },
-                virt_text_win_col = start_col,
-                priority = 200 + iteration
+                virt_text_win_col = start_col * buffers[bufnr].tabstop,
+                priority = 100 + iteration
             })
         end
 
@@ -53,13 +54,14 @@ local function color_node(start_row, start_col, end_row, end_col, iteration)
 end
 
 ---Highlight all the nodes under the input node
+---@param bufnr integer
 ---@param node TSNode the node currently at
 ---@param iteration integer How deep we are in the recursive cycle
 ---@param prev_start_row integer
 ---@param prev_start_col integer
 ---@param prev_end_row integer
 ---@return integer largest_col
-local function block(node, iteration, prev_start_row, prev_start_col, prev_end_row)
+local function block(bufnr, node, iteration, prev_start_row, prev_start_col, prev_end_row)
     -- The largest col nr
     local largest_col = 0
 
@@ -80,7 +82,7 @@ local function block(node, iteration, prev_start_row, prev_start_col, prev_end_r
     end
 
     -- Get all the lines for node
-    local lines = api.nvim_buf_get_lines(0, start_row, end_row, true)
+    local lines = api.nvim_buf_get_lines(bufnr, start_row, end_row, true)
     local longest_line = find_biggest_end_col(lines)
 
     -- Update longest_col to the biggest out of all children or longest line
@@ -93,7 +95,7 @@ local function block(node, iteration, prev_start_row, prev_start_col, prev_end_r
 
     -- Go into child node unless no children are found
     for child_node in node:iter_children() do
-        local child_largest_col = block(child_node, iteration + 1, start_row, start_col, end_row)
+        local child_largest_col = block(bufnr, child_node, iteration + 1, start_row, start_col, end_row)
 
         -- Update largest end col
         largest_col = math.max(largest_col, child_largest_col)
@@ -102,7 +104,7 @@ local function block(node, iteration, prev_start_row, prev_start_col, prev_end_r
     -- print all the data for the node: Start, End, and width (end col)
     -- print(string.format("start_row: %d, start_col %d, largest_end_col %d, end_row: %d, iteration: %d, type: %s", start_row, start_col, largest_col, end_row, iteration, node:type()))
 
-    color_node(start_row, start_col, end_row, largest_col, iteration)
+    color_node(bufnr, start_row, start_col, end_row, largest_col, iteration)
     return largest_col + 1
 end
 
@@ -117,9 +119,9 @@ local function update(bufnr)
     if #trees == 0 then return end -- Seems an already Blocked buffer might result in this returning nil-- Seems an already Blocked buffer might result in this returning nil
     local ts_node = trees[1]:root()
 
-    vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
+    vim.api.nvim_buf_clear_namespace(bufnr, ns_id, 0, -1)
     for c in ts_node:iter_children() do
-        block(c, nest_amount + 1, -1, -1, -1)
+        block(bufnr, c, nest_amount + 1, -1, -1, -1)
     end
 end
 
@@ -130,29 +132,32 @@ local function add_buff_and_start(bufnr)
         buffers[bufnr] = {}
         buffers[bufnr].parser = parser
 
+        -- Add the tabstop to the buffer
+        local expandtab = vim.bo[bufnr].expandtab
+        local a = 1
+        if not expandtab then
+            a = vim.lsp.util.get_effective_tabstop(bufnr)
+        end
+        buffers[bufnr].tabstop = a
+
+
+        -- set matchpairs to empty string to avoid conflicts with treesitter
+        buffers[bufnr].matchpairs = vim.bo[bufnr].matchpairs
+        vim.bo[bufnr].matchpairs = ""
+
+        -- Initialize the buffer and update it
+        vim.print(buffers[bufnr].tabstop)
         vim.schedule(function()
             update(bufnr)
         end)
-
         -- Autocommand that checks the
-
-        api.nvim_buf_attach(bufnr, false, {
-            on_lines = function(lines, buff_handle, changed_tick, first_line, last_line)
+        parser:register_cbs({
+            on_changedtree = function()
                 vim.schedule(function()
                     update(bufnr)
                 end)
             end
-        })
-
-        --        parser:register_cbs({
-        --            on_changedtree = function()
-        --                vim.schedule(function()
-        --                    update(bufnr)
-        --                end)
-        --            end
-        --        }, false)
-
-        -- We dont care about coloring the root node since thats the entire buffer
+        }, false)
     end
 end
 
@@ -170,6 +175,7 @@ function M.off()
     vim.api.nvim_buf_clear_namespace(bufnr, ns_id, 0, -1)
     if buffers[bufnr] then
         buffers[bufnr].parser:register_cbs({ on_changedtree = function() end }) -- Register an empty function to remove the previous callback
+        vim.bo.matchpairs = buffers[bufnr].matchpairs
         buffers[bufnr] = nil
     end
 end
